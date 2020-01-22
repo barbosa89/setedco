24625e9342b2e8c9ee4d90e6dbfb293057c793ae
barbosa <omar.barbosa89@outlook.com>
2020-01-22 15:50:12 -0500

    SEO optimization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,13 @@
     <link href="{{ asset('theme/images/icon.png') }}" rel="shortcut icon" type="image/x-icon">
     <link rel="canonical" href="{{ config('app.url') }}">
 
-    <meta name="description" content="CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, consultoría, asesorías, energía solar fotovoltaica.">
-    <meta name="keywords" content="CCTV, sistemas de seguridad, redes, telecomunicaciones, mantenimiento a computador, mantenimiento a impresoras, redes eléctricas, generadores de energía, consultoría, asesorías, energía solar fotovoltaica, instalaciones eléctricas,  tableros de distribución, sistemas puestas a tierra, transformadores eléctricos, variadores de velocidad,  motores eléctricos, plantas eléctricas, bombas, aires acondicionados ">
+    <meta name="description" content="CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, energía solar fotovoltaica.">
+    <meta name="keywords" content="CCTV, sistemas de seguridad, redes, telecomunicaciones, mantenimiento a computador, mantenimiento a impresoras, redes eléctricas, generadores de energía, energía solar fotovoltaica, instalaciones eléctricas,  tableros de distribución, sistemas puestas a tierra, transformadores eléctricos, variadores de velocidad,  motores eléctricos, plantas eléctricas, bombas, aires acondicionados ">
     <meta name="author" content="{{ config('app.name') }} - {{ config('app.fullname') }}">
 
     <meta property="og:title" content="{{ config('app.name') }} - {{ config('app.fullname') }}">
-    <meta property="og:description" content="CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, consultoría, asesorías, energía solar fotovoltaica.">
-    <meta property="og:image" content="{{ asset('theme/images/icon.png') }}">
+    <meta property="og:description" content="CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, energía solar fotovoltaica.">
+    <meta property="og:image" content="{{ asset('theme/images/icons/icon-384x384.png') }}">
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ config('app.url') }}">
     <meta property="og:site_name" content="{{ config('app.name') }}">
@@ -43,9 +43,28 @@
             "alternateName": "{{ config('app.fullname') }}",
             "url": "{{ config('app.url') }}",
             "image": "{{ asset('theme/images/icon.png') }}",
-            "description": "CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, consultoría, asesorías, energía solar fotovoltaica."
+            "description": "CCTV, sistemas electrónicos de seguridad, telecomunicaciones, redes eléctricas, generadores de energía, automatización y control, energía solar fotovoltaica."
         }
     </script>
+    <script type="application/ld+json">
+        {
+          "@context" : "http://schema.org",
+          "@type" : "Organization",
+          "name" : "{{ config('app.name') }}",
+         "url" : "{{ config('app.url') }}",
+         "sameAs" : [
+           "https://www.facebook.com/SETEDCO-INGENIERIA-557599321285008/",
+           "https://www.instagram.com/setedco_ingenieria/"
+           ],
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Bucaramanga",
+            "addressRegion": "BGA",
+            "postalCode": "680001",
+            "addressCountry": "CO"
+          }
+        }
+        </script>
     <!-- Smartsupp Live Chat script -->
     <script type="text/javascript" async>
         var _smartsupp = _smartsupp || {};
