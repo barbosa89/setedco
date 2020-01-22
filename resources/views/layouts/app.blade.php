bda8e07537a61b0ae72eb4e60a7b7e1dc43c2875
barbosa <omar.barbosa89@outlook.com>
2020-01-22 13:15:06 -0500

    Chat Smartsupp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,5 +46,16 @@
             "description": "Description"
         }
     </script>
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript" async>
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'b6c8cbd0463a1ce3dd8eb090848092ffc042f103';
+        window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+        })(document);
+    </script>
 </body>
 </html>
