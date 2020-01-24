b5c8a761ab855f25a44fb150cd9e190bbab5157d
barbosa <omar.barbosa89@outlook.com>
2020-01-24 13:55:33 -0500

    Lazy load image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -380,7 +380,7 @@
                     </div>
                 </div>
             </div>
-            <div class="da-contact" id="contact">
+            <div class="da-contact" id="contact" data-background="{{ asset('theme/images/staticmap.png') }}">
                 <div class="da-contact-detail" data-aos="zoom-in" data-aos-duration="1000">
                     <div class="container">
                         <div class="card py-4 px-4">
