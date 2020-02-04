c04b376a79e0d1da673e16f309dfb1522d727a8a
barbosa <omar.barbosa89@outlook.com>
2020-02-04 10:54:34 -0500

    Change Smartsupp code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,16 +36,17 @@
     </script>
 
     <!-- Smartsupp Live Chat script -->
-    <script type="text/javascript" async>
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
         var _smartsupp = _smartsupp || {};
-        _smartsupp.key = 'b6c8cbd0463a1ce3dd8eb090848092ffc042f103';
+        _smartsupp.key = '61777d13efe36fa9151368e98452223bff944dbc';
         window.smartsupp||(function(d) {
         var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
         s=d.getElementsByTagName('script')[0];c=d.createElement('script');
         c.type='text/javascript';c.charset='utf-8';c.async=true;
         c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
         })(document);
-    </script>
+        </script>
 </head>
 <body id="top">
     @include('flash::message')
